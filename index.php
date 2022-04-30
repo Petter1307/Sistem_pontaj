1b3aa6b3d7a35901d205feae9ecea805e9f4444d
Petter1307 <petrealupu75@gmail.com>
2022-04-30 21:38:43 +0300

    Initial modifications

--- index.php
+++ index.php
@@ -1,58 +1,38 @@
-<?php
-session_start();
- ?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-    <meta charset="utf-8">
-    <title>Proiect</title>
-</head>
-
-<body>
-    <div class="signup_wrapper">
-        <form action="includes/singup.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username"><br>
-            <input type="text" name="email" placeholder="Email"><br>
-            <input type="text" name="first_name" placeholder="Prenume"><br>
-            <input type="text" name="last_name" placeholder="Name"><br>
-            <input type="password" name="pwd" placeholder="Parola"><br>
-            <input type="password" name="pwdRepeat" placeholder="Repeta parola"><br>
-            <input type="radio" name="radio_type" value="Profesor">
-            <label for="profesor">Profesor</label><br>
-            <input type="radio" name="radio_type" value="Student" checked>
-            <label for="student">Student</label><br>
-            <br>
-            <button type="submit" name="submit">Sing Up</button>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Pontaj</title>
+  </head>
+  <body>
+    <div id="page_wrapper">
+      <header>
+        <img src="../img/sigla_uab_big.png" alt="Sigla uab" class="login_img" />
+      </header>
+      <div id="login_form_wrapper">
+        <form action="../includes/login.inc.php" method="post" id="login_form">
+          <input
+            type="text"
+            name="username"
+            placeholder="Username sau email"
+            class="login_input"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            class="login_input"
+          />
+          <input type="submit" name="submit" value="LOGIN" id="login_button" />
         </form>
 
-        <form action="includes/login.inc.php" method="post">
-            <input type="text" name="uid">
-            <input type="password" name="pwd">
-            <input type="submit" name="submit" value="Login">
-        </form>
-
-        <?php 
-                if(isset($_SESSION['error'])){
-                    $error = $_SESSION['error'];
-                    $_SESSION['error'] = null;
-                    echo "error from sesion: ".$error;
-                }
-                     if(isset($_SESSION['id'])){
-                    $error = $_SESSION['id'];
-                    echo "error from sesion: ".$error;
-                }
-        
-        ?>
-
+        <div id="forgot_password">
+          <p><a href="index.html">Forgot password ?</a></p>
+        </div>
+      </div>
     </div>
-    <nav>
-        <ul>
-            <li><a href="pages/login.html">Login page</a></li>
-            <li><a href="pages/home.html"></a></li>
-            <li><a href=""></a></li>
-        </ul>
-    </nav>
-</body>
-
+  </body>
 </html>
