6c052fc6988f37b0c059cb158e624bb283902d3c
Petter1307 <petrealupu75@gmail.com>
2022-05-03 23:22:04 +0300

    added users methods

--- classes/user.class.php
+++ classes/user.class.php
@@ -2,19 +2,76 @@
 class User extends Login
 {
     private $id;
-    private $user_name;
+    private $first_name;
+    private $last_name;
+    private $email;
     private $user_type;
+    private $id_card;
+    private $creation_time;
 
-    public function __construct($id, $name, $type)
+    public function __construct($id)
     {
-        $this->id = $id;
-        $user_name = $name;
-        $user_type = $type;
+        $stmt = $this->connect()->prepare('SELECT * FROM user where id=?');
+        if (!$stmt->execute(array($id))) {
+            $stmt = null;
+            $this->id = 0;
+            $this->first_name = null;
+            $this->last_name = null;
+            $this->email = null;
+            $this->user_type = 'student';
+            $this->id_card = 0;
+            $this->creation_time = null;
+            exit();
+        }
+        if ($stmt->rowCount() == 0) {
+            $stmt = null;
+            $this->id = 0;
+            $this->first_name = null;
+            $this->last_name = null;
+            $this->email = null;
+            $this->user_type = 'student';
+            $this->id_card = 0;
+            $this->creation_time = null;
+            exit();
+        }
+        $tmp_user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->id = $tmp_user['id'];
+        $this->first_name = $tmp_user['first_name'];
+        $this->last_name = $tmp_user['last_name'];
+        $this->email = $tmp_user['email'];
+        $this->user_type = $tmp_user['user_type'];
+        $this->id_card = $tmp_user['id_card'];
+        $this->creation_time = $tmp_user['user_creation_time'];
+
     }
 
-    public function getUserData($id)
+    public function getUserId()
     {
-        //TODO  Create views that gets all user information needed.
+        return $this->id;
+    }
+    public function getUserFirst()
+    {
+        return $this->first_name;
+    }
+    public function getUserLast()
+    {
+        return $this->last_name;
+    }
+    public function getUserEmail()
+    {
+        return $this->email;
+    }
+    public function getUserType()
+    {
+        return $this->user_type;
+    }
+    public function getUserIdCard()
+    {
+        return $this->id_card;
+    }
+    public function getUserCreationTime()
+    {
+        return $this->creation_time;
     }
 
 }
--- includes/user.inc.php
+++ includes/user.inc.php
@@ -1,3 +1,2 @@
 <?php
-
-?>
+include "../classes/user.class.php";
